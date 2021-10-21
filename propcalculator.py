974baf54520732fd26f731d9c5cd9a4d7828c3bf
htauL <80499850+htauL@users.noreply.github.com>
2021-10-21 15:08:14 -0300

    Update propcalculator.py

--- propcalculator.py
+++ propcalculator.py
@@ -1,6 +1,7 @@
 exit = False
-print('This is the personal proportion calculator from htauL \n\n a/b = c/d \n\n Mode = discover one term.\n\n')
-while exit == False:
+print('This is the personal proportion calculator from htauL \n\n a/b = c/d\n')
+
+def discover():
 	term = input('Which term you want to find?\n\n[a]   [c]\n--- = ---\n[b]   [d]\n\nChoose one: ').upper().strip()
 	if term == 'A':
 		b = float(input('b = '))
@@ -29,3 +30,27 @@ while exit == False:
 		c = float(input('c = '))
 		d = (b*c)/a
 		print(f'{term} is equal to {d}\n')	
+			
+			
+def compare():
+	print('Please insert the terms of the proportion:\n\n[a]   [c]\n--- = ---\n[b]   [d]\n\n')
+	a = float(input('a = '))
+	b = float(input('b = '))
+	c = float(input('c = '))
+	d = float(input('d = '))
+	
+	result = (a/b)/(c/d)
+	
+	if result == 1:
+		print('Proportions are equal\n\n')
+	else:
+		print('Proportions are not equal\n\n')
+	
+	
+while exit == False:
+	mode = input('Which mode do you want to use?\n\n[1] Discover one term\n[2] Compare proportions\n\nChoose one: ')
+	if mode == '1':
+		discover()
+	if mode == '2':
+		compare()
+	
